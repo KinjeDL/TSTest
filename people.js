c2e632786594eebe287f9ed11854977e7a7d6686
KinjeDL <lordkinje@gmail.com>
2019-07-26 11:20:51 -0400

    Test

--- people.js
+++ people.js
@@ -1,7 +1,24 @@
-import {Eve} from "../people";
+// import {Eve} from "../people";
 
 export function outputEve() {
     document.getElementById("girl-output").innerHTML = Eve.describeface();
 }
 
 outputEve();
+
+export class person {
+    name: string;
+    
+    describeface() {
+        return "A Face";
+    }
+    
+    constructor(name: string) {
+        this.name=name;
+    }
+
+}
+
+export const people = {
+    Eve: new person("Eve")
+}
